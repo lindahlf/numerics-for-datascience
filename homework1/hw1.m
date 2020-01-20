21af2633004f97d70aa2686ae04cd43d7f2ec0ba
Fred <lindahl.fred@gmail.com>
2020-01-20 14:47:21 +0100

    finished code for q. 2 and 4

--- homework1/hw1.m
+++ homework1/hw1.m
@@ -1,3 +1,82 @@
+%% Part 2b
 clear all; close all; clc
 
-V = []; Q0 = []; R = []; A0 = V;
+V = [1 2 2003 2005; 2 2 2002 2004; 3 2 2001 2003; 4 7 7005 7012];
+
+[m n] = size(V);
+Q = []; R = []; A = V; p = min(m,n); A_error = [];
+
+for i = 1:p
+    q = A(:,i)/norm(A(:,i));
+    rt = q'*A;
+    Q = [Q q];
+    R = [R; rt];
+    A = A - q*rt;
+    A_error = [A_error norm(A)];
+end
+
+plot([1:1:p],A_error)
+
+%% Part 2c
+clear all; close all; clc
+
+V = load_mat_hw1(1000,100);
+
+[m n] = size(V);
+Q = []; R = []; A = V; p = min(m,n); A_error = [];
+
+for i = 1:p
+    q = A(:,i)/norm(A(:,i));
+    rt = q'*A;
+    Q = [Q q];
+    R = [R; rt];
+    A = A - q*rt;
+    A_error = [A_error norm(A)];
+end
+
+plot([1:1:p],A_error)
+
+%% Part 2d
+clear all; close all; clc
+
+V = load_mat_hw1(1000,100);
+
+[m n] = size(V);
+Q = []; R = []; A = V; p = min(m,n); A_error = [];
+
+for j = 1:p
+    [~, i] = max(vecnorm(A));
+    q = A(:,i)/norm(A(:,i));
+    rt = q'*A;
+    Q = [Q q];
+    R = [R; rt];
+    A = A - q*rt;
+    A_error = [A_error norm(A)];
+end
+
+plot([1:1:p],A_error)
+
+%% Part 4
+clear all; close all; clc
+
+W = load_mat_hw1(1000,100);
+
+[m n] = size(W);
+Q = []; R = []; A = W; p = min(m,n); 
+
+k = 1;
+
+while norm(A) > 10e-10
+    [~, i] = max(vecnorm(A));
+    q = A(:,i)/norm(A(:,i));
+    rt = q'*A;
+    Q = [Q q];
+    R = [R; rt];
+    A = A - q*rt;
+    k = k + 1;
+end
+
+[U_hat, S, V] = svd(R);
+U = Q*U_hat;
+
+
